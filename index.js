8890b564539e2451c5e419eb7babb069fc4820ab
pprachit09 <pprachit09@gmail.com>
2018-09-13 22:45:38 +0530

    Adding initial codebase

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/quit', function(req, res){
+    res.send('Bye Express');
+});
+
+var server = app.listen(9000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Your app is running at http://%s:%s", host, port);
+});
